e2e862a02356437269a8a690dc63af9817485012
Hunt Zhan <huntzhan.dev@gmail.com>
2018-09-25 03:58:08 +0800

    Bump version: 0.1.0 → 0.1.1

--- setup.py
+++ setup.py
@@ -133,7 +133,7 @@ setup(
         test_suite='tests',
         tests_require=test_requirements,
         url='https://github.com/cnt-dev/cnt.sam',
-        version='0.1.0',
+        version='0.1.1',
         zip_safe=False,
         # pybind11.
         ext_modules=ext_modules,
